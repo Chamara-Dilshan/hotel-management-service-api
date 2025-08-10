41c7be248146cb1008fb88df2caf3e098b1da507
Chamara Dilshan <chamaradilshan0011@gmail.com>
2025-08-11 03:38:56 +0530

    updated

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/entity/Hotel.java
@@ -0,0 +1,43 @@
+package com.cpd.hotel_system.hotel_management_service_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+    
+    @Id
+    @Column (name = "hotel_id")
+    private String hotelId;
+
+    @Column(name = "hotel_name", nullable = false, length = 100)
+    private String hotelName;
+
+    @Column(name = "star_rating", nullable = false)
+    private int starRating;
+
+    @Column(nullable = false)
+    @Lob
+    private Blob description; //smart text area
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "active_status")
+    private boolean activeStatus;
+
+    @Column(name = "starting_from")
+    private BigDecimal startingFrom;
+}
