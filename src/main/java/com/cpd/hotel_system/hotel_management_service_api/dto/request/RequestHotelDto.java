ab67daf5f312bdba83a139a3e480ad18df4e8dd7
Chamara Dilshan <chamaradilshan0011@gmail.com>
2025-08-12 00:06:51 +0530

    Added Dto

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/dto/request/RequestHotelDto.java
@@ -0,0 +1,23 @@
+package com.cpd.hotel_system.hotel_management_service_api.dto.request;
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class RequestHotelDto {
+    private String hotelName;
+    private String description;
+    private int starRating;
+    private BigDecimal startingFrom;
+
+;
+}
---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/dto/request/RequestRoomImageDto.java
@@ -0,0 +1,20 @@
+package com.cpd.hotel_system.hotel_management_service_api.dto.request;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class RequestRoomImageDto {
+    private MultipartFile file;
+    private String roomId;
+}
