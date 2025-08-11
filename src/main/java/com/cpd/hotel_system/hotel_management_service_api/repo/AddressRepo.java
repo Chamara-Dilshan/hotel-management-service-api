2ccef4c3de8683c80bcdfb09e4f1109c29aaab3e
Chamara Dilshan <chamaradilshan0011@gmail.com>
2025-08-11 21:54:34 +0530

    added repo

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/repo/AddressRepo.java
@@ -0,0 +1,10 @@
+package com.cpd.hotel_system.hotel_management_service_api.repo;
+
+import com.cpd.hotel_system.hotel_management_service_api.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, String> {
+    // Custom query methods (if any) can be added here
+}
---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/repo/BranchRepo.java
@@ -0,0 +1,10 @@
+package com.cpd.hotel_system.hotel_management_service_api.repo;
+
+import com.cpd.hotel_system.hotel_management_service_api.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepo extends JpaRepository<Branch, String> {
+    // Custom query methods (if any) can be added here
+}
